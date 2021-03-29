ec539c2d7f3c680cc33a365b3a65f20b6cbf6a7f
Eva <yw4147@nyu.edu>
2021-03-29 23:32:35 +0800

    Midterm final version!

---
+++ Assets/Lecture3/Scripts/ending.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ending : MonoBehaviour
+{
+    //public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        //if(other.CompareTag("Player")){
+           // camera.GetComponent<cameraFollow>().endingmove = true;
+   //     }
+    }
+}
